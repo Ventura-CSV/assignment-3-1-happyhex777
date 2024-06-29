8b4b80980f67b7134224ad57fe87141927684557
Hector Diaz <hhectordiazz@icloud.com>
2024-06-28 20:07:40 -0700

    Completed assignment 3-1

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ def main():
     Code Your Program here
     ########################################
     """
+    if number1<number2:
+        if number1<number3:
+            min_value=number1
+        else:
+            min_value=number3
+    else:
+        if number2<number3:
+            min_value=number2
+        else:
+            min_value=number3
+            
+    print(number1,number2,number3)
+    print(min_value)
 
     ########################################
     # Do not delete the return statement
